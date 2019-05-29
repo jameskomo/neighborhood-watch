549e97a670b1a075c2c71f2e537b38dbd8732501
jameskomo <james.komoh@gmail.com>
2019-05-29 17:33:33 +0300

    Initial Project repository created, dependencies installed and Django app and project created

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/komo/Documents/moringa-school/Python/Django/neighborhood_watch/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
