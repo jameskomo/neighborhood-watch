0bc46bbfc18a721b932d594d59208def5efa0b09
jameskomo <james.komoh@gmail.com>
2019-05-30 11:04:41 +0300

    Neighborhood tuple created in users model to allow users to select neighborhood on registration. models object passed to Registration and Update forms

--- users/forms.py
+++ users/forms.py
@@ -2,10 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from users.models import neighborhood
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    neighborhood = forms.ChoiceField(choices=neighborhood)
 
     class Meta:
         model = User
@@ -14,10 +16,12 @@ class UserRegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+    neighborhood = forms.ChoiceField(choices=neighborhood)
+    
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email',]
 
 
 class ProfileUpdateForm(forms.ModelForm):
